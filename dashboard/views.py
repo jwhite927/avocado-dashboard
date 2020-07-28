a284caca068eb997294a751c9810d0eb6a1b5228
JoshWhite <jwhite927@gmail.com>
2020-07-28 09:07:13 -0400

    feat: Print buttons from a list

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stats import (
+    load_data,
+    get_time_stats,
+    get_station_stats,
+    get_trip_duration_stats,
+    get_user_stats,
+    CITY_DATA,
+)
 
 def index(request):
-    return render(request, 'dashboard/home.html')
+	context = {
+		'cities': [city.title() for city in CITY_DATA.keys()],
+	}
+	return render(request, 'dashboard/home.html', context)
